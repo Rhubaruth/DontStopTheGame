0b506114a1a62d3dc7ed0d2ea3a73afc2ce122c1
Rhubaruth <kubik.vana@gmail.com>
2023-09-30 16:38:30 +0200

    custom tk

--- main_custom.py
+++ main_custom.py
@@ -1,25 +1,25 @@
-import tkinter as tk
+import customtkinter as ctk
 
 
-root = tk.Tk()
+root = ctk.CTk()
+root.title("Gayme")
 root.geometry("600x480")
-root.resizable(False, False)
 
 def get_input(*args):
     user_input = entry.get().strip()
     if not user_input:
         user_input = "[No Input]"
-    result_label.config(text=f'{user_input}')
+    result_label."configure(text=f'{user_input}')
 
 # entry widget
-bottom_frame = tk.Frame(root, bg="MediumPurple3")
-bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
+bottom_frame = ctk.CTkFrame(root)
+bottom_frame.pack(side=ctk.BOTTOM, fill=ctk.X)
 
-entry = tk.Entry(bottom_frame)
+entry = ctk.CTkEntry(bottom_frame)
 entry.grid(row=0, column=0, sticky="ew", padx=15, pady=5)
 entry.bind('<Return>', get_input)
 
-submit_button = tk.Button(bottom_frame, text="Submit", command=get_input)
+submit_button = ctk.CTkButton(bottom_frame, text="Submit", command=get_input)
 submit_button.grid(row=0, column=1, padx=15, pady=5)
 
 # configure the column 0
@@ -27,14 +27,13 @@ bottom_frame.columnconfigure(0, weight=1)
 bottom_frame.columnconfigure(0, weight=1)
 
 # result label widget
-result_label = tk.Label(root, text="[No Input]")
-result_label.pack(side=tk.TOP, pady=10, fill="x")
+result_label = ctk.CTkLabel(root, text="[No Input]")
+result_label.pack(side=ctk.TOP, pady=10, fill="x")
 
 
 
 def main():
     # Start the Tkinter main loop
-    root.title("Gayme")
     root.mainloop()
 
 
