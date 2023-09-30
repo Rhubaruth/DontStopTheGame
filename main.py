d56d61b76a351eceea61252fbcfaa648581b1cac
Rhubaruth <kubik.vana@gmail.com>
2023-09-30 13:50:06 +0200

    bottom panel with pack

--- main.py
+++ main.py
@@ -2,18 +2,36 @@ import tkinter as tk
 
 
 root = tk.Tk()
-root.geometry("480x320")
+root.geometry("600x480")
 root.resizable(False, False)
 
-# Frame with player buttons
-player_frame = tk.Frame(root, bg="lightgreen", width=200)
-player_frame.pack(side=tk.LEFT, fill="y")
+def get_input():
+    user_input = entry.get().strip()
+    if not user_input:
+        user_input = "[No Input]"
+    result_label.config(text=f'{user_input}')
 
-def game():
+# entry widget
+bottom_frame = tk.Frame(root)
+bottom_frame.pack(side=tk.BOTTOM, fill="x")
+
+entry = tk.Entry(bottom_frame)
+entry.pack(side=tk.LEFT, pady=10, fill="x")
+
+submit_button = tk.Button(bottom_frame, text="Submit", command=get_input)
+submit_button.pack(side=tk.RIGHT, padx=15, pady=10)
+
+# result label widget
+result_label = tk.Label(root, text="[No Input]")
+result_label.pack(side=tk.TOP, pady=10, fill="x")
+
+
+
+def main():
     # Start the Tkinter main loop
     root.title("Gayme")
     root.mainloop()
 
 
 if __name__ == "__main__":
-    game()
+    main()
